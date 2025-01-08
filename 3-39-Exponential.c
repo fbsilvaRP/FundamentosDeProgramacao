372f5cafa157b314d6a60a32024e84c9c2e8d0d7
Felipe <168025584+fbsilvaRP@users.noreply.github.com>
2025-01-07 23:07:56 -0300

    Add files via upload

---
+++ 3-39-Exponential.c
@@ -0,0 +1,17 @@
+/*Faça um programa que realize a operação x^y, digitados pelo usuário*/
+#include <stdio.h>
+int main()
+{
+	int op = 1; 
+	while(op == 1)
+	{
+		int x, y; 
+		printf("\n* * * Calculando x ^ y * * *");
+		printf("\nDigite o valor de x: ");
+		scanf("%d", &x); 
+		printf("Agora, digite o valor de y: "); 
+		scanf("%d", &y);
+		//  ** ** * * *Continuar o pograma...   
+	  
+	}
+}
